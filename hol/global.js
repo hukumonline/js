4e4372fb729d0429a93240a56451b97fa4290a71
nihki <nihki@hukumonline.com>
2011-09-09 15:01:59 +0700

    modified global.js

--- hol/global.js
+++ hol/global.js
@@ -1,6 +1,5 @@
 $(document).ready(function()
 {
-    //$("#featured > ul").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true);
 	$("#featured > ul").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true);
 	$("#featured > ul").hover(
 		function() {
@@ -16,7 +15,22 @@ $(document).ready(function()
     $("#news").newsTicker();
     
 
-    $('#tabs').tabs();
+	//When page loads...
+	$(".tab_content").hide(); //Hide all content
+	$("ul.tabs li:first").addClass("active").show(); //Activate first tab
+	$(".tab_content:first").show(); //Show first tab content
+
+	//On Click Event
+	$("ul.tabs li").click(function() {
+
+		$("ul.tabs li").removeClass("active"); //Remove any "active" class
+		$(this).addClass("active"); //Add "active" class to selected tab
+		$(".tab_content").hide(); //Hide all tab content
+
+		var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+		$(activeTab).fadeIn(); //Fade in the active ID content
+		return false;
+	});
     
     //slideShow();
 });
